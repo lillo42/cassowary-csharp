ecfb98c644fd99d9af8bba473509f1a1b6000b13
Rafael Lillo Andrade <rafael.andrade@justeattakeaway.com>
2023-04-09 09:14:15 +0100

    fix gitversion

--- build/Build.cs
+++ build/Build.cs
@@ -36,7 +36,7 @@ class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion(NoFetch = true, Framework = "net6.0")] readonly GitVersion GitVersion;
+    [GitVersion(NoFetch = true, Framework = "net5.0")] readonly GitVersion GitVersion;
     [CI] GitHubActions GitHubActions;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
